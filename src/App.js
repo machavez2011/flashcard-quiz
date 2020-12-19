5581215b480cd22d015a555748a453340a07c864
Michael Chavez <machavez2011@gmail.com>
2020-12-18 21:40:13 -0700

    App pulls data from opentdb database and populates cards on load.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css'
+import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeStr(questionItem.correct_answer);
+
+          const options = [...questionItem.incorrect_answers.map(a => decodeStr(a)), answer];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeStr(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <div>
       <FlashcardList flashcards={flashcards} />
     </div>
   );
+}
+
+function decodeStr(str) {
+  let textArea = document.createElement('textarea');
+  textArea.innerHTML = str;
+  return textArea.value;
 }
 
 const SAMPLE_FLASHCARDS = [
