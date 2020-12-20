56250f6aabd6bda7874d066529bf73ba9e3a6cc0
Michael Chavez <machavez2011@gmail.com>
2020-12-19 19:15:55 -0700

    Cards now automatically resize based on screen size.

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function Flashcard(props) {
     const [flip, setFlip] = useState(false);
+    const [height, setHeight] = useState('initial');
+
+    const frontEl = useRef();
+    const backEl = useRef();
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height;
+        const backHeight = backEl.current.getBoundingClientRect().height;
+        setHeight(Math.max(frontHeight, backHeight, 100));
+    }
+
+    useEffect(setMaxHeight, [props.flashcard.question, props.flashcard.answer, props.flashcard.options]);
+
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight);
+        return () => window.removeEventListener('resize', setMaxHeight); 
+    }, [])
 
     return (
         <div 
             className={`card ${flip ? 'flip' : ''}`}
+            style={{ height: height }}
             onClick={() => setFlip(!flip)}
         >
-            <div className='front'>
+            <div className='front' ref={frontEl}>
                 {props.flashcard.question}
                 <div className='flashcard-options'>
                     {props.flashcard.options.map(option => {
@@ -16,7 +34,7 @@ export default function Flashcard(props) {
                     })}
                 </div>
             </div>
-            <div className='back'>{props.flashcard.answer}</div>
+            <div className='back' ref={backEl}>{props.flashcard.answer}</div>
         </div>
     )
 }
